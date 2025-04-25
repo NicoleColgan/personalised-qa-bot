cea0354cbe43e8017f818b7f8a9786820900b319
Colgan <nicole.colgan@hpe.com>
2025-04-25 09:53:27 +0100

    requirements and pdf file

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-#todo:
+# Set up
+## Install requirements
+```
+pip install -r requirements.txt
+```
+# todo:
 - upoad file to cli & answer questions based on it
 - validate inputs
+- requirements.txt file
 - swap out clis
 - create ui
+- move generate requirements to new script???
--- main.py
+++ main.py
@@ -13,19 +13,29 @@ def load_secrets():
 def load_documents(file_path):
     print("Loading docs")
     if file_path.endswith(".txt"):
+        print(f"processing {file_path}")
         loader = TextLoader(file_path)
+    elif file_path.endswith(".pdf"):
+        print(f"processing {file_path}")
+        loader = PyPDFLoader(file_path)
     else:
         raise ValueError("Unsupported file type. Please use .txt or .pdf files")
     return loader.load()
 
-
+def generate_requirements():
+    print("Generating requirements file")
+    os.system("pip freeze > requirements.txt")
 
 def main():
-    load_secrets()
+    # load_secrets()
     file_path = input("enter file path of file (.txt or .pdf): ")
     question = input("Enter your question: ")
 
-    docs = load_documents("docs/about_me.txt")
+    docs = load_documents("docs/about_me_pdf.pdf")
+
+    #produce requirements.txt
+    #probably doesnt need to be done every time
+    #generate_requirements()
 
 
 
